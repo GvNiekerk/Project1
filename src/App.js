973a7ffd78ba3ab4460f48d73e09ea09c07a9997
GvNiekerk <gvniekerk011@gmail.com>
2020-09-30 22:48:35 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
         <h1 className="Menu-Header" style={{ color: "#FFFFFF", fontSize: 45 }}>EXPERIENCE</h1>
       </div>
       <div id="projects" className="Light-Section">
-        <h1 className="Menu-Header" style={{ color: "#c60f0f", fontSize: 45, width : 100 }}>PROJECTS</h1>
+        <h1 className="Menu-Header" style={{ color: "#c60f0f", fontSize: 45, width : 100, minHeight : 500 }}>PROJECTS</h1>
       </div>
       <div id="contact" className="Dark-Section">
         <h1 className="Menu-Header" style={{ color: "#FFFFFF", fontSize: 45 }}>CONTACT</h1>
